0446b5c0ce65fbf38b3077c74553833c96a4da4f
LakshayShakya <lakshayshakya12@gmail.com>
2026-02-02 15:53:44 +0530

    Add files via upload

---
+++ experiment2/main2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class main2 {
+    public static void checkPrime(int sum,int n){
+        int count=0;
+        for(int i=0; i<n; i++){
+            
+        }
+
+    }
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int sum=0;
+    for(int i=0; i<a; i++){
+        sum += sc.nextInt();
+        checkPrime(sum,a);
+    }
+
+}
+}
